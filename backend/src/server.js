986b21d15f4cd5ed90818e944648a3eed75ce0c6
Andrew-sudo-create <andrewkriegler.ak@gmail.com>
2025-02-10 16:54:40 +0200

    trying to fix cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -11,7 +11,8 @@ const app = express();
 //Middleware
 app.use(cors());
 app.use(express.json()); //Parse JSON requests
-app.use(cors({ origin: "https://cms-mk5rrz2l5-andrewsudocreates-projects.vercel.app/" })); // Allow all domains
+app.use(express.urlencoded({ extended: true })); // Handle form data
+app.use(cors({ origin: "https://cms-fa7459jvn-andrewsudocreates-projects.vercel.app/" })); // Allow all domains
 
 app.get('/', (req, res) => {
   res.send('CMS Backend is running...');
