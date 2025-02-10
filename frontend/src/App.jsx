71ee71cf4f7f1427c5cdbf63e4164a636f9778dc
Andrew-sudo-create <andrewkriegler.ak@gmail.com>
2025-02-10 17:10:52 +0200

    adding data fetcher component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,30 +1,14 @@
-import { useEffect,useState } from "react";
-// import DataFetcher from "./components/DataFetcher";
+
 import './App.scss'
+import DataFetcher from "./components/DataFetcher";
 
 function App() {
-  const [fetchedData, setFetchedData] = useState([]);
-  console.log(import.meta.env.VITE_API_URL)
-  console.log(import.meta.env.VITE_API_URL)
-  useEffect(() => {
-    fetch(import.meta.env.VITE_API_URL + "api/data")
-      .then((res) => res.json())
-      .then((data) => {
-        console.log("Fetched Data:", data.data); // Check what's inside data
-        setFetchedData(data.data);
-      })
-      .catch((error) => console.error("Error fetching data:", error));
-  }, []);
 
   return (
     <>
       <div>
       <h1>My CMS Frontend</h1>
-      {fetchedData && fetchedData.length > 0 ? (
-      fetchedData.map((item) => <p key={item.id}>{item.title}</p>)
-    ) : (
-      <p>Loading or no data available...</p>
-    )}
+      <DataFetcher/>
   
     </div>
   
